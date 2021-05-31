8f34bf91588051785ef69939708c07dbb0bc2144
Tomás Jiménez <tjimenez999@gmail.com>
2021-05-31 18:38:01 -0400

    fix

--- receptor.cpp
+++ receptor.cpp
@@ -26,9 +26,6 @@ BYTE tempByteArr[4];
 BYTE timeByteArr[4];
 extern int dataQty = 0;
 int main() {
-  if (start) {
-    start =false;
-  }
   if (wiringPiSetup() == -1)
     exit(1);
   piHiPri(99);
@@ -39,27 +36,31 @@ int main() {
     printf("Unable to start interrupt function\n");
   }
   while (true) {
-  while (nbytes < 10)
-    delay(10);
-
-  generateReceivedFrame(receivedFrame);
-  if (receivedFrame.cmd == 4) {
-    printf("Closing...\n");
-    exit(1);
-  } else if (receivedFrame.cmd == 2) {
-    for (int j = 0; j<4;j++) {
-      tempByteArr[j] = receivedFrame.data[j];
-      timeByteArr[j] = receivedFrame.data[j+4];
+    while (nbytes < 10) {
+      printf("nbytes %d", nbytes);
+      delay(10);
     }
-    int temp = 0;
-    int time = 0;
-    getIntegerOfByteArray(tempByteArr, &temp);
-    getIntegerOfByteArray(timeByteArr, &time);
-    tempArr[dataQty] = temp;
-    timeArr[dataQty] = time;
-    printf("Se han capturado %d datos\n", dataQty);
-  }
-  nbytes = 0;
+    generateReceivedFrame(receivedFrame);
+    if (receivedFrame.cmd == 4) {
+      printf("Closing...\n");
+      exit(1);
+    } else if (receivedFrame.cmd == 2) {
+      for (int j = 0; j<4;j++) {
+        tempByteArr[j] = receivedFrame.data[j];
+        timeByteArr[j] = receivedFrame.data[j+4];
+      }
+      int temp = 0;
+      int time = 0;
+      getIntegerOfByteArray(tempByteArr, &temp);
+      getIntegerOfByteArray(timeByteArr, &time);
+      tempArr[dataQty] = temp;
+      timeArr[dataQty] = time;
+      printf("Se han capturado %d datos\n", dataQty);
+    }
+    receivedFrame.cmd = NULL;
+    receivedFrame.length = NULL;
+    receivedFrame.data = NULL;
+    nbytes = 0;
   }
 }
 
