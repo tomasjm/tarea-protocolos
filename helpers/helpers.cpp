83f34c7a91c42e0d07a0b57735bbe76de6ace3df
Tomás Jiménez <tjimenez999@gmail.com>
2021-05-31 21:17:22 -0400

    fix

--- helpers/helpers.cpp
+++ helpers/helpers.cpp
@@ -13,7 +13,7 @@ void readSensorData(int q, int valuesArr[], int timesArr[]) {
   for (int i = 0; i<q; i++) {
   fp = popen("cat /sys/bus/w1/devices/28-0113126a6baa/w1_slave | grep -i -o \"t=[0-9]*\" | grep -o \"[0-9]*\"", "r");
   fscanf(fp, "%d", &rawTemp);
-  valuesArr[i] = rawTemp+10;
+  valuesArr[i] = rawTemp+10000;
   timesArr[i] = time(NULL);
   printf("Se han tomado %d datos de %d\n", i+1, q);
   printf("Temp %d\n", rawTemp);
